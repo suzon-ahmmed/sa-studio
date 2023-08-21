6bddd59e70e5a41d52a2c9334f00a8b8d33c3dc8
suzon-ahmmed <info.suzonahamed@gmail.com>
2023-08-21 22:43:36 +0600

    Feature - add page-2 design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-function combine() {
+function init() {
   gsap.registerPlugin(ScrollTrigger);
 
   const locoScroll = new LocomotiveScroll({
@@ -30,38 +30,61 @@ function combine() {
 
   ScrollTrigger.refresh();
 }
-combine();
 
-let curser = document.querySelector(".curser");
+init();
+
+let cursor = document.querySelector(".cursor");
 let main = document.querySelector(".main");
 
-main.addEventListener("mousemove", (dets) => {
-  curser.style.left = dets.x - 5 + "px";
-  curser.style.top = dets.y - 8 + "px";
+window.addEventListener("mousemove", (dest) => {
+  // circle.style.transform = `translate(${dest.clientX}px, ${dest.clientY}px)`;
+  gsap.to(cursor, {
+    x: dest.clientX - 10,
+    y: dest.clientY - 10,
+    duration: 0.4,
+    ease: Expo,
+  });
 });
 
-let vdo = document.querySelector(".video-wrapper video");
+// const p2 = document.querySelector(".page-2");
+
+// p2.addEventListener("mousemove", () => {
+//   cursor.classList.add("active");
+// });
+// p2.addEventListener("mouseleave", () => {
+//   cursor.innerText = "active";
+//   cursor.classList.remove("active");
+// });
+
+let vdo = document.querySelector("video");
 
 vdo.addEventListener("click", () => {
-  console.log("is clicked?");
-  if (vdo.muted = false) {
+  // console.log("is clicked?");
+  if (vdo.muted == false) {
     vdo.muted = true;
     // or toggle class, style it with a volume icon sprite, change background-position
   } else {
     vdo.muted = false;
   }
 });
+vdo.addEventListener("mousemove", () => {
+  cursor.classList.add("video");
+});
+vdo.addEventListener("mouseleave", () => {
+  cursor.classList.remove("video");
+});
 
 gsap.set(".page-1 .content", { filter: "blur(4px)" });
 gsap.from(".page-1 .content h1, .page-1 .content h2", {
-  y: 8,
-  rotate: 5,
+  y: 30,
+  // rotate: 5,
   opacity: 0,
   filter: "blur(0px)",
   delay: 0.3,
   duration: 0.7,
   stagger: 0.1,
 });
+
 gsap.from(".page-1 .content .subtext", {
   y: 50,
   opacity: 0,
@@ -93,15 +116,6 @@ st.to(
   ".page-1 .content h1",
   {
     x: -200,
-    //   duration: 1,
-    // scrollTrigger: {
-    //   trigger: ".page-1 h1",
-    //   scroller: ".main",
-    //   markers: true,
-    //   start: "top 30%",
-    //   end: "top 0",
-    //   scrub: 3,
-    // },
   },
   "page1"
 );
@@ -119,3 +133,50 @@ st.to(
   },
   "page1"
 );
+
+let st2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page-1",
+    scroller: ".main",
+    start: "top -130%",
+    end: "top -110",
+    scrub: 3,
+  },
+});
+
+st2.to(
+  ".main",
+  {
+    backgroundColor: "#fff",
+    color: "#000",
+  },
+  "page2"
+);
+st2.to(
+  "nav",
+  {
+    color: "#000",
+  },
+  "page2"
+);
+st2.to(
+  ".circle",
+  {
+    backgroundColor: "#000",
+  },
+  "page2"
+);
+
+
+const rows = document.querySelectorAll(".row .text-content");
+const imageHolderImg = document.querySelector(".img-content img");
+
+rows.forEach((row) => {
+  row.addEventListener("mouseenter", function () {
+    // const link = this.querySelector("a");
+    // // console.log(link)
+    // link.style.opacity = "1";
+    const value = this.getAttribute("data-src");
+    imageHolderImg.setAttribute("src", value);
+  });
+});
